8eaa53092c5f7115a27c0e28bb5da6f4589573c5
Jamie Martines <dev.jpm@gmail.com>
2020-02-27 21:01:52 -0500

    basic loading, two random players, added alt for image

--- src/App.js
+++ src/App.js
@@ -5,31 +5,31 @@ import Player from './components/Player'
 
 class App extends React.Component {
   state = {
+      loading: true,
       players: []
   }
   componentDidMount() {
     fetchPlayers()
       .then((players)=>{
         this.setState({
-          players: players
+          players: players,
+          loading: false
         })
       })
   }
   render(){
-    // let players = this.state.players
+    let players = this.state.players
+    let playerOne = players[Math.floor(Math.random()*players.length)]
+    let playerTwo = players[Math.floor(Math.random()*players.length)]
+
+    if (this.state.loading === true) {
+      return <h1>loading</h1>
+    }
     return (
-      
       <div className="App">
         <ul>
-          {
-            this.state.players.map((player)=>{
-              return (
-                <li key={player.id}>
-                  <Player player={player} />
-                </li>
-              )
-            })
-          }
+            <li><Player player={playerOne}/></li>
+            <li><Player player={playerTwo}/></li>
        </ul>
       </div>
     );
--- src/components/Player.js
+++ src/components/Player.js
@@ -4,7 +4,7 @@ function Player({ player }) {
     const {first_name, last_name, images, fppg} = player
     return (
         <div>
-            <img src={images.default.url} width={images.default.width} height={images.default.height}/>
+            <img alt={`Img for ${first_name} ${last_name}`} src={images.default.url} width={images.default.width} height={images.default.height}/>
             <h1>{first_name} {last_name}</h1>
             <h2>{fppg}</h2>
         </div>
