772b85f9a7736192ae04f686bac8fd1f362a7b62
Jamie Martines <dev.jpm@gmail.com>
2020-02-29 20:45:01 -0500

    set win condition, added restart

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
     playerTwo: null,
     selected: null,
     count: 0,
-    status: false, 
+    status: false,
   }
   componentDidMount() {
     fetchPlayers()
@@ -32,23 +32,32 @@ class App extends React.Component {
       status: false
     })
   }
+  restart = () => {
+    this.setState({
+      playerOne: this.state.players[Math.floor(Math.random() * this.state.players.length)],
+      playerTwo: this.state.players[Math.floor(Math.random() * this.state.players.length)],
+      selected: null,
+      status: false,
+      count: 0
+    })
+  }
   handleClick = (playerInput, playerNumber) => {
     // console.log('click')
     console.log(playerNumber, ' selected')
     // battle
     let results = battle(this.state.playerOne, this.state.playerTwo)
     // show
-    this.setState({selected: playerInput})
+    this.setState({ selected: playerInput })
     // set winner
-    if(results[0] === playerInput){
+    if (results[0] === playerInput) {
       console.log('winner')
       // show as winner
       // increment
       this.setState({
         status: true,
-        count: this.state.count+1
+        count: this.state.count + 1
       })
-    }else{
+    } else {
       console.log('loser')
       this.setState({
         status: false
@@ -63,18 +72,25 @@ class App extends React.Component {
     }
     return (
       <div className="App">
-        <h1>Select the better player</h1>
-        <h3>Correct Count: {count}</h3>
-        <ul>
-          <li><Player player={playerOne} onSelection={this.handleClick} playerNumber={'playerOne'} selectClass={this.state.selected === playerOne ? 'active' : null} /></li>
-          <li><Player player={playerTwo} onSelection={this.handleClick} playerNumber={'playerTwo'} selectClass={this.state.selected === playerTwo ? 'active' : null} /></li>
-        </ul>
-        {this.state.status ? (
-          <h1 className='winner'>Winner</h1>
+        {this.state.count < 10 ? (
+          <React.Fragment>
+            <h1>Select the better player</h1>
+            <h3>Correct Count: {count}</h3>
+            <ul>
+              <li><Player player={playerOne} onSelection={this.handleClick} playerNumber={'playerOne'} selectClass={this.state.selected === playerOne ? 'active' : null} /></li>
+              <li><Player player={playerTwo} onSelection={this.handleClick} playerNumber={'playerTwo'} selectClass={this.state.selected === playerTwo ? 'active' : null} /></li>
+            </ul>
+            {this.state.status ?
+              (<h1 className='winner'>Winner</h1>) :
+              (<h1 className='loser'>Loser</h1>)}
+            <button onClick={this.reset}>Next Players</button>
+          </React.Fragment>
         ) : (
-          <h1 className='loser'>Loser</h1>
+          <React.Fragment>
+            <h1>Congrats you got 10 correct</h1>
+            <button onClick={this.restart}>Restart</button>
+          </React.Fragment>
         )}
-        <button onClick={this.reset}>Next Players</button>
       </div>
     );
   }
